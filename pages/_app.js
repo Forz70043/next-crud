e9d70bebe277810ed2793005897d25a1c852a100
Forz70043 <alfonso.pisicchio@libero.it>
2022-07-22 20:52:14 +0200

    Added import global style to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { useEffect } from "react";
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
     useEffect(() => {
