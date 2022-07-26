4a84fa98151e42837b2c17180d4ceb2b7fe9f961
Forz70043 <alfonso.pisicchio@libero.it>
2022-07-26 16:06:49 +0200

    Update index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Layout from "../components/layout";
 import Homepage from "../components/homepage";
+import { getUsers } from "../lib/users";
+
+let users = getUsers();
 
 export default function Home() {
   return (
