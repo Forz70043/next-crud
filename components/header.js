6d39349e5bfb2500deb63b0d90e46e55dd170861
Forz70043 <alfonso.pisicchio@libero.it>
2023-01-05 13:49:00 +0100

    Added icons for pwa

--- components/header.js
+++ components/header.js
@@ -11,7 +11,8 @@ export default function Header() {
             <meta name="description" content="Description" />
             <meta name="keywords" content="Keywords" />
             <meta name="theme-color" content="#317EFB" />
-
+            <meta name="apple-mobile-web-app-status-bar" content="#317EFB" />
+            
             <link rel="manifest" href="/manifest.json" />
             <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
             <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
